548e37feb53a65a3ed48ef0a5ebab3ca709a7f13
落笔 <46271592+Skymly@users.noreply.github.com>
2026-05-09 00:01:05 +0800

    添加项目文件。

---
+++ R3.SourceGenerators.Samples/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using R3.ObservableEvents;
+
+using System.Windows;
+namespace R3.SourceGenerators.Samples;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.ObservableEvents()
+            .MouseMove()
+            .ThrottleFirst(TimeSpan.FromMilliseconds(50))
+            .Subscribe(x => Content = x.GetPosition(this));
+    }
+}
